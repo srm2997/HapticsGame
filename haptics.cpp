d2bcbcc5fb11f101cd609290cf1d0fb35c0d2b29
Sam <srm2997@rit.edu>
2012-03-26 15:40:02 -0400

    Addes actual source files

---
+++ haptics.cpp
@@ -0,0 +1,265 @@
+#include "haptics.h"
+#include <windows.h>
+#include <math.h>
+#include <sstream>
+
+// Continuous servo callback function
+HDLServoOpExitCode ContactCB(void* pUserData)
+{
+    // Get pointer to haptics object
+    HapticsClass* haptics = static_cast< HapticsClass* >( pUserData );
+
+    // Get current state of haptic device
+    hdlToolPosition(haptics->m_positionServo);
+    hdlToolButton(&(haptics->m_buttonServo));
+
+    // Call the function that does the heavy duty calculations.
+    haptics->cubeContact();
+
+    // Send forces to device
+    hdlSetToolForce(haptics->m_forceServo);
+
+    // Make sure to continue processing
+    return HDL_SERVOOP_CONTINUE;
+}
+
+// On-demand synchronization callback function
+HDLServoOpExitCode GetStateCB(void* pUserData)
+{
+    // Get pointer to haptics object
+    HapticsClass* haptics = static_cast< HapticsClass* >( pUserData );
+
+    // Call the function that copies data between servo side 
+    // and client side
+    haptics->synch();
+
+    // Only do this once.  The application will decide when it
+    // wants to do it again, and call CreateServoOp with
+    // bBlocking = true
+    return HDL_SERVOOP_EXIT;
+}
+
+// Constructor--just make sure needed variables are initialized.
+HapticsClass::HapticsClass( double& xposb, double& yposb)
+    : m_lastFace(FACE_NONE),
+      m_deviceHandle(HDL_INVALID_HANDLE),
+      m_servoOp(HDL_INVALID_HANDLE),
+      m_cubeEdgeLength(1),
+      m_cubeStiffness(1),
+      m_inited(false),
+	  m_xpos(xposb),
+	  m_ypos(yposb),
+	  dobump(false)
+{
+    for (int i = 0; i < 3; i++)
+        m_positionServo[i] = 0;
+}
+
+// Destructor--make sure devices are uninited.
+HapticsClass::~HapticsClass()
+{
+    uninit();
+}
+
+
+
+void HapticsClass::init(double a_cubeSize, double a_stiffness, double paddleWidth )
+{
+	m_paddleWidth = paddleWidth;
+    m_cubeEdgeLength = a_cubeSize;
+    m_cubeStiffness = a_stiffness;
+	//m_xpos = xposb;
+	//m_ypos = yposb;
+
+
+    HDLError err = HDL_NO_ERROR;
+
+    // Passing "DEFAULT" or 0 initializes the default device based on the
+    // [DEFAULT] section of HDAL.INI.   The names of other sections of HDAL.INI
+    // could be passed instead, allowing run-time control of different devices
+    // or the same device with different parameters.  See HDAL.INI for details.
+    m_deviceHandle = hdlInitNamedDevice("DEFAULT");
+    testHDLError("hdlInitDevice");
+
+    if (m_deviceHandle == HDL_INVALID_HANDLE)
+    {
+        MessageBox(NULL, "Could not open device", "Device Failure", MB_OK);
+        exit(0);
+    }
+
+    // Now that the device is fully initialized, start the servo thread.
+    // Failing to do this will result in a non-funtional haptics application.
+    hdlStart();
+    testHDLError("hdlStart");
+
+    // Set up callback function
+    m_servoOp = hdlCreateServoOp(ContactCB, this, bNonBlocking);
+    if (m_servoOp == HDL_INVALID_HANDLE)
+    {
+        MessageBox(NULL, "Invalid servo op handle", "Device Failure", MB_OK);
+    }
+    testHDLError("hdlCreateServoOp");
+
+    // Make the device current.  All subsequent calls will
+    // be directed towards the current device.
+    hdlMakeCurrent(m_deviceHandle);
+    testHDLError("hdlMakeCurrent");
+
+    // Get the extents of the device workspace.
+    // Used to create the mapping between device and application coordinates.
+    // Returned dimensions in the array are minx, miny, minz, maxx, maxy, maxz
+    //                                      left, bottom, far, right, top, near)
+    // Right-handed coordinates:
+    //   left-right is the x-axis, right is greater than left
+    //   bottom-top is the y-axis, top is greater than bottom
+    //   near-far is the z-axis, near is greater than far
+    // workspace center is (0,0,0)
+    hdlDeviceWorkspace(m_workspaceDims);
+    testHDLError("hdlDeviceWorkspace");
+
+
+    // Establish the transformation from device space to app space
+    // To keep things simple, we will define the app space units as
+    // inches, and set the workspace to approximate the physical
+    // workspace of the Falcon.  That is, a 4" cube centered on the
+    // origin.  Note the Z axis values; this has the effect of
+    // moving the origin of world coordinates toward the base of the
+    // unit.
+    double gameWorkspace[] = {-2,-2,-2,2,2,3};
+    bool useUniformScale = true;
+    hdluGenerateHapticToAppWorkspaceTransform(m_workspaceDims,
+                                              gameWorkspace,
+                                              useUniformScale,
+                                              m_transformMat);
+    testHDLError("hdluGenerateHapticToAppWorkspaceTransform");
+
+
+    m_inited = true;
+}
+
+// uninit() undoes the setup in reverse order.  Note the setting of
+// handles.  This prevents a problem if uninit() is called
+// more than once.
+void HapticsClass::uninit()
+{
+    if (m_servoOp != HDL_INVALID_HANDLE)
+    {
+        hdlDestroyServoOp(m_servoOp);
+        m_servoOp = HDL_INVALID_HANDLE;
+    }
+    hdlStop();
+    if (m_deviceHandle != HDL_INVALID_HANDLE)
+    {
+        hdlUninitDevice(m_deviceHandle);
+        m_deviceHandle = HDL_INVALID_HANDLE;
+    }
+    m_inited = false;
+}
+
+// This is a simple function for testing error returns.  A production
+// application would need to be more sophisticated than this.
+void HapticsClass::testHDLError(const char* str)
+{
+    HDLError err = hdlGetError();
+    if (err != HDL_NO_ERROR)
+    {
+        MessageBox(NULL, str, "HDAL ERROR", MB_OK);
+        abort();
+    }
+}
+
+// This is the entry point used by the application to synchronize
+// data access to the device.  Using this function eliminates the 
+// need for the application to worry about threads.
+void HapticsClass::synchFromServo()
+{
+    hdlCreateServoOp(GetStateCB, this, bBlocking);
+}
+
+// GetStateCB calls this function to do the actual data movement.
+void HapticsClass::synch()
+{
+    // m_positionApp is set in cubeContact().
+    m_buttonApp = m_buttonServo;
+}
+
+// A utility function to handle matrix multiplication.  A production application
+// would have a full vector/matrix math library at its disposal, but this is a
+// simplified example.
+void HapticsClass::vecMultMatrix(double srcVec[3], double mat[16], double dstVec[3])
+{
+    dstVec[0] = mat[0] * srcVec[0] 
+        + mat[4] * srcVec[1]
+        + mat[8] * srcVec[2]
+        + mat[12];
+    
+    dstVec[1] = mat[1] * srcVec[0]
+        + mat[5] * srcVec[1]
+        + mat[9] * srcVec[2]
+        + mat[13];
+
+    dstVec[2] = mat[2] * srcVec[0]
+        + mat[6] * srcVec[1]
+        + mat[10] * srcVec[2]
+        + mat[14];
+}
+
+void HapticsClass::bump(){
+	dobump = true;
+}
+
+// Here is where the heavy calculations are done.  This function is
+// called from ContactCB to calculate the forces based on current
+// cursor position and cube dimensions.  A simple spring model is
+// used.
+void HapticsClass::cubeContact()
+{
+    // Convert from device coordinates to application coordinates.
+    vecMultMatrix(m_positionServo, m_transformMat, m_positionApp);
+
+    m_forceServo[0] = 0; 
+    m_forceServo[1] = 0; 
+    m_forceServo[2] = 0;
+
+    // Skip the whole thing if not initialized
+    if (!m_inited) return;
+
+
+//	char letters[100];
+//	sprintf( letters, "%f", m_xpos  );
+//	OutputDebugString( letters );
+//	OutputDebugString("\n" );
+
+	double d = m_xpos * m_xpos + m_ypos * m_ypos;
+
+
+    // add spring stiffness to force effect
+    m_forceServo[0] += (m_positionApp[0] - m_xpos - m_paddleWidth * 1.5) * -5;
+    m_forceServo[1] += (m_positionApp[1] - m_ypos) * -5;
+}
+
+// Interface function to get current position
+void HapticsClass::getPosition(double pos[3])
+{
+    pos[0] = m_positionApp[0];
+    pos[1] = m_positionApp[1];
+    pos[2] = m_positionApp[2];
+
+}
+
+// Interface function to get button state.  Only one button is used
+// in this application.
+bool HapticsClass::isButtonDown()
+{
+    return m_buttonApp;
+}
+
+// For this application, the only device status of interest is the
+// calibration status.  A different application may want to test for
+// HDAL_UNINITIALIZED and/or HDAL_SERVO_NOT_STARTED
+bool HapticsClass::isDeviceCalibrated()
+{
+    unsigned int state = hdlGetState();
+
+    return ((state & HDAL_NOT_CALIBRATED) == 0);
+}
---
+++ main_opengl.cpp
@@ -0,0 +1,431 @@
+// Copyright 2007 Novint Technologies, Inc. All rights reserved.
+// Available only under license from Novint Technologies, Inc.
+//
+//
+// Basic application showing use of HDAL.
+// Haptic interaction with a ball.
+// Graphic display of cursor.
+//
+//
+// Comment the following line to get a console window on startup
+#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
+
+// windows.h must precede glut.h to eliminate the "exit" compiler error in VS2003, VS2005
+#include <windows.h>
+#include "glut.h"
+#include <math.h>
+#include "haptics.h"
+#include <sstream>
+
+
+double NORTH = 1.0;
+double SOUTH = -1.0;
+double EAST = 1.5;
+double WEST = -1.5;
+
+int SCORE_P1 = 0;
+int SCORE_P2 = 0;
+
+double xposb, yposb;
+double yposp1, xposp1;
+double yposp2, xposp2;
+double xmov, ymov;
+SYSTEMTIME lasttime;
+
+
+
+// Cube parameters
+const double gStiffness = 200.0;
+const double gCubeEdgeLength = 0.5;
+
+// Some OpenGL values
+static GLuint gCursorDisplayList = 0;
+static double gCursorRadius = 0.05;
+static GLfloat colorRed[]  = {1.0, 0.0, 0.0};
+static GLfloat colorTeal[] = {0.0, 0.5, 0.5};
+static GLfloat* gCurrentColor;
+
+// The haptics object, with which we must interact
+HapticsClass gHaptics(xposb,yposb);
+
+// Forward declarations
+void glutDisplay(void);
+void glutReshape(int width, int height);
+void glutIdle(void);
+void glutKeyboard(unsigned char key, int x, int y);
+void exitHandler(void);
+void initGL();
+void initScene();
+void drawGraphics();
+void drawCursor();
+
+void glutMouseMove(int x, int y);
+
+
+
+int main(int argc, char *argv[])
+{
+	AllocConsole();
+    // Normal OpenGL Setup
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Basic--OpenGL");
+    glutDisplayFunc(glutDisplay);
+    glutReshapeFunc(glutReshape);
+    glutIdleFunc(glutIdle);
+    glutKeyboardFunc(glutKeyboard);
+	glutPassiveMotionFunc(glutMouseMove);
+
+    
+    // Set up handler to make sure teardown is done.
+    atexit(exitHandler);
+
+    // Set up the scene with graphics and haptics
+    initScene();
+
+    // Now start the simulation
+    glutMainLoop();
+
+    return 0;
+}
+
+// drawing callback function
+void glutDisplay()
+{   
+    drawGraphics();
+    glutSwapBuffers();
+}
+
+// reshape function (handle window resize)
+void glutReshape(int width, int height)
+{
+    static const double kPI = 3.1415926535897932384626433832795;
+    static const double kFovY = 40;
+
+    double nearDist, farDist, aspect;
+
+    glViewport(0, 0, width, height);
+
+    nearDist = 1.0 / tan((kFovY / 2.0) * kPI / 180.0);
+    farDist = nearDist + 2.0;
+    aspect = (double) width / height;
+   
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(kFovY, aspect, nearDist, farDist);
+
+    /* Place the camera down the Z axis looking at the origin */
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();            
+    gluLookAt(0, 0, nearDist + 2.0,
+              0, 0, 0,
+              0, 1, 0);
+}
+
+// What to do when doing nothing else
+void glutIdle()
+{
+    glutPostRedisplay();
+}
+
+
+// Handle keyboard.  In this case, just the Escape key
+void glutKeyboard(unsigned char key, int x, int y)
+{
+    static bool inited = true;
+
+    if (key == 27) // esc key
+    {
+        exit(0);
+    }
+}
+
+// Handle mouse movement
+void glutMouseMove( int x, int y){
+	yposp2 = (y - 250) / -(500 / 3.0);
+}
+
+// Scene setup
+void initScene()
+{
+
+	xposp1 = EAST + gCubeEdgeLength / 4.0;
+	yposp1 = 0;
+	xposp2 = WEST - gCubeEdgeLength / 4.0;
+	yposp2 = 0;
+	xposb = 0;
+	yposb = 0;
+	xmov = 0.7;
+	ymov = 0.7;
+
+    // Call the haptics initialization function
+    gHaptics.init(gCubeEdgeLength, gStiffness, gCubeEdgeLength / 2);
+
+    // Set up the OpenGL graphics
+    initGL();
+
+    // Some time is required between init() and checking status,
+    // for the device to initialize and stabilize.  In a complex
+    // application, this time can be consumed in the initGL()
+    // function.  Here, it is simulated with Sleep().
+    Sleep(100);
+
+    // Tell the user what to do if the device is not calibrated
+    if (!gHaptics.isDeviceCalibrated())
+        MessageBox(NULL, 
+                   // The next two lines are one long string
+                   "Please home the device by extending\n"
+                   "then pushing the arms all the way in.",
+                   "Not Homed",
+                   MB_OK);
+
+
+	GetSystemTime(&lasttime);
+}
+
+// Set up OpenGL.  Details are left to the reader
+void initGL()
+{
+    static const GLfloat light_model_ambient[] = {0.3f, 0.3f, 0.3f, 1.0f};
+    static const GLfloat light0_diffuse[] = {0.9f, 0.9f, 0.9f, 0.9f};   
+    static const GLfloat light0_direction[] = {0.0f, -0.4f, 1.0f, 0.0f};    
+    
+    /* Enable depth buffering for hidden surface removal. */
+    glDepthFunc(GL_LEQUAL);
+    glEnable(GL_DEPTH_TEST);
+    
+    /* Cull back faces. */
+    glCullFace(GL_BACK);
+    glEnable(GL_CULL_FACE);
+    
+    /* Set lighting parameters */
+    glEnable(GL_LIGHTING);
+    glEnable(GL_NORMALIZE);
+    glShadeModel(GL_SMOOTH);
+    
+    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_FALSE);
+    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);    
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, light_model_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
+    glLightfv(GL_LIGHT0, GL_POSITION, light0_direction);
+    glEnable(GL_LIGHT0);   
+}
+
+// Make sure we exit cleanly
+void exitHandler()
+{
+    gHaptics.uninit();
+}
+
+float CalculateTimeLeft(SYSTEMTIME timeTo, SYSTEMTIME timeFrom )
+{
+    LARGE_INTEGER nFrom, nTo;
+    FILETIME timeTemp;
+    float fReturn = 0.0f;
+ 
+    SystemTimeToFileTime(&timeFrom, &timeTemp);
+    nFrom.HighPart = timeTemp.dwHighDateTime;
+    nFrom.LowPart = timeTemp.dwLowDateTime;
+ 
+    SystemTimeToFileTime(&timeTo, &timeTemp);
+    nTo.HighPart = timeTemp.dwHighDateTime;
+    nTo.LowPart = timeTemp.dwLowDateTime;
+ 
+    if (nTo.QuadPart > nFrom.QuadPart)
+    {
+        unsigned long long nDiff = nTo.QuadPart - nFrom.QuadPart;
+        fReturn = nDiff / 10000.0f / 1000.0f / 60.0f / 60.0f / 24.0f;
+    }
+     
+    return fReturn;
+}
+
+long double Delta(const SYSTEMTIME st1, const SYSTEMTIME st2)
+{
+    union timeunion {
+        FILETIME fileTime;
+        ULARGE_INTEGER ul;
+    } ;
+    
+    timeunion ft1;
+    timeunion ft2;
+
+    SystemTimeToFileTime(&st1, &ft1.fileTime);
+    SystemTimeToFileTime(&st2, &ft2.fileTime);
+
+    return (long double)( ft2.ul.QuadPart - ft1.ul.QuadPart ) / 10000.0; // milliseconds
+}
+
+void Score(){
+	char letters[100];
+	sprintf( letters, "%i - %i", SCORE_P2, SCORE_P1  );
+	OutputDebugString( letters );
+	OutputDebugString("\n" );
+}
+
+void BoundCheck( long double i ){
+
+	double halfEdge = gCubeEdgeLength / 2.0;
+
+	double top = yposb + halfEdge ;
+	double bottom = yposb - halfEdge;
+	double left = xposb - halfEdge;
+	double right = xposb + halfEdge;
+
+//	if( right > EAST ){
+//		xposb -= xmov * 2 * i / 1000.0;
+//		xmov = -xmov;
+//	}
+	if( top > NORTH ){
+		yposb -= ymov * 2 * i / 1000.0;
+		ymov = -ymov;
+	}
+//	if( left < WEST ){
+//		xposb -= xmov * 2 * i / 1000.0;
+//		xmov = -xmov;
+//	}
+	if( bottom < SOUTH ){
+		yposb -= ymov * 2 * i / 1000.0;
+		ymov = -ymov;
+
+	}
+
+	if( right >= EAST ){
+		if( top > yposp1 - halfEdge && bottom < yposp1 + halfEdge ){
+			xposb -= xmov * 2 * i / 1000.0;
+			xmov = -xmov;
+			xmov *= 1.1;
+			ymov *= 1.1;
+		}else{
+			xposb = 0;
+			xmov = 0.7;
+			ymov = 0.7;
+			SCORE_P2++;
+			Score();
+		}
+	}
+
+	if( left <= WEST ){
+		if( top > yposp2 - halfEdge && bottom < yposp2 + halfEdge ){
+			xposb -= xmov * 2 * i / 1000.0;
+			xmov = -xmov;
+			xmov *= 1.1;
+			ymov *= 1.1;
+		}else{
+			xposb = 0;
+			xmov = -0.7;
+			ymov = 0.7;
+			SCORE_P1++;
+			Score();
+		}
+	}
+}
+
+void UpdatePos(){
+	SYSTEMTIME str_t;
+	GetSystemTime(&str_t);
+	
+	long double i = Delta( lasttime, str_t );
+
+//	char letters[100];
+//	sprintf( letters, "%f", i  );
+//	OutputDebugString( letters );
+//	OutputDebugString("\n" );
+
+	xposb += xmov * i / 1000.0;
+	yposb += ymov * i / 1000.0;
+
+    BoundCheck( i );
+
+//	sprintf( letters, "%f", xmov * i / 1000 );
+//	OutputDebugString( letters );
+//	OutputDebugString("\n" );
+
+
+
+	lasttime = str_t;
+}
+
+// Draw the cursor and the cube.  In a real application,
+// this function would be much more complex.
+void drawGraphics()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);           
+
+    drawCursor();
+
+	
+	double cp[3];
+	gHaptics.synchFromServo();
+	gHaptics.getPosition( cp );
+
+	yposp1 = cp[1];
+
+	// Draw right paddle (p1)
+	glTranslatef( xposp1, yposp1, 0 );
+	glScalef( 0.5, 1, 1 );
+	glutSolidCube(gCubeEdgeLength);
+	glScalef( 2.0, 1, 1 );
+	glTranslatef( -xposp1, -yposp1, 0 );
+
+	// Draw left paddle (p2)
+	glTranslatef( xposp2, yposp2, 0 );
+	glScalef( 0.5, 1, 1 );
+	glutSolidCube(gCubeEdgeLength);
+	glScalef( 2.0, 1, 1 );	
+	glTranslatef( -xposp2, -yposp2, 0 );
+
+	// Draw puck
+    glTranslatef( xposb, yposb, 0);
+    glutSolidCube(gCubeEdgeLength);
+	glTranslatef(-xposb, -yposb, 0);
+
+	// Update puck position
+	UpdatePos();
+}
+
+// Draw the cursor
+void drawCursor()
+{
+    static const int kCursorTess = 15;
+
+    // Haptic cursor position in "world coordinates"
+    double cursorPosWC[3];
+
+    // Must synch before data is valid
+    gHaptics.synchFromServo();
+    gHaptics.getPosition(cursorPosWC);
+
+    // The color will depend on the button state.
+    gCurrentColor = gHaptics.isButtonDown() ? colorRed : colorTeal;
+
+    GLUquadricObj *qobj = 0;
+
+    glPushAttrib(GL_CURRENT_BIT | GL_ENABLE_BIT | GL_LIGHTING_BIT);
+    glPushMatrix();
+
+    // Page 505-506 of The Red Book recommends using a display list
+    // for static quadrics such as spheres
+    if (!gCursorDisplayList)
+    {
+        gCursorDisplayList = glGenLists(1);
+        glNewList(gCursorDisplayList, GL_COMPILE);
+        qobj = gluNewQuadric();
+
+        gluSphere(qobj, gCursorRadius, kCursorTess, kCursorTess);
+        
+        gluDeleteQuadric(qobj);
+        glEndList();
+    }
+
+    glTranslatef(cursorPosWC[0], cursorPosWC[1], cursorPosWC[2]);
+
+    glEnable(GL_COLOR_MATERIAL);
+    glColor3fv(gCurrentColor);
+
+    glCallList(gCursorDisplayList);
+
+    glPopMatrix(); 
+    glPopAttrib();
+}
